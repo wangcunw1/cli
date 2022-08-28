135d7a224ff1bc460389d0b23176352be82cef87
wcw1qqw <109303568+wcw1qqw@users.noreply.github.com>
2022-08-28 18:05:11 +0800

    Update a.sh

--- a.sh
+++ a.sh
@@ -3,13 +3,13 @@ echo "Enter Your VM Name:"
 read vmname
 az network vnet create \
   --name $vmname"Net" \
-  --resource-group learn-bc35e0aa-27ff-4078-bcba-9032fea619ea \
+  --resource-group learn-a240a501-f0a4-4ab1-aa3a-d7dbdff4af25 \
   --address-prefixes 10.0.0.0/16 \
   --subnet-name $vmname"SubNet" \
   --subnet-prefixes 10.0.0.0/24
  
  az vm create \
-  --resource-group learn-bc35e0aa-27ff-4078-bcba-9032fea619ea \
+  --resource-group learn-a240a501-f0a4-4ab1-aa3a-d7dbdff4af25 \
   --size DS2_v2\
   --name $vmname \
   --image UbuntuLTS \
